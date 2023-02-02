685e4a56df0e3476906d5cfecda591e7c1e096d5
Arafat <islamarafat171@gmail.com>
2023-02-02 17:42:12 +0100

    Still working on google analytics.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,24 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { ApolloProvider } from "@apollo/client"
 import client from "../apollo-client"
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-KJF12P63GL"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-KJF12P63GL');
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </ApolloProvider>
   )
